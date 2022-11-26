085dd17ded6cffc43a7789177412f6a90280e727
Dario Cruz <rio2012@gmail.com>
2022-11-26 10:26:02 -0500

    Add webpack packages.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hey is this working????')
+// Importing all needed content.
+import './style.css'
+
+
+const mainPage = () => {
+    const navBar = document.createElement('div').className('navbar')
+    
+}
