06866b6ffda83b07c448bf16b0315841d5bc0b47
Dario Cruz <rio2012@gmail.com>
2022-11-28 10:07:05 -0500

    Add custom vars for colors and applied to navBar.

---
+++ src/pages/mainpage.js
@@ -0,0 +1,11 @@
+import './mainpage.css'
+import whiteLogo from '../logo/carhhouse-white.webp'
+
+
+const renderMainpage = () => {
+    const mainPageDiv = document.createElement('div')
+    
+
+}
+
+export {renderMainpage}
