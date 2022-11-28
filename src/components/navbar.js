cf3710b0413014b31a0997959487a78139784d22
Dario Cruz <rio2012@gmail.com>
2022-11-27 19:59:53 -0500

    Organize code.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,37 +5,35 @@ import whiteLogo from '../logo/carbhouse-white.svg'
 const renderNav = () => {
     // Define elements to be added.
     const navBar = document.createElement('div')
-    navBar.setAttribute('class', 'navbar')
-
+    const leftDiv = document.createElement('div')
+    const rightDiv = document.createElement('div')
+    const navMenu = document.createElement('div')
+    const navDirections = document.createElement('div')
+    const navAbout = document.createElement('div')
     const myBlackLogo = new Image()
+    
+    // Define Attributes
+    navBar.setAttribute('class', 'navbar')
     myBlackLogo.src = blackLogo
     myBlackLogo.setAttribute('id', 'logo')
-    
-    const leftDiv = document.createElement('div')
     leftDiv.setAttribute('class', 'left-side')
-    leftDiv.appendChild(myBlackLogo)
-    
-    const rightDiv = document.createElement('div')
     rightDiv.setAttribute('class', 'right-side')
-
-    const navMenu = document.createElement('div')
     navMenu.setAttribute('class', 'menu')
-    navMenu.innerHTML = `<h1 class="menu">Menu</h1>`
-    rightDiv.appendChild(navMenu)
-
-    const navDirections = document.createElement('div')
     navDirections.setAttribute('class', 'directions')
-    navDirections.innerHTML = `<h1 class="directions">Directions</h1>`
-    rightDiv.appendChild(navDirections)
-
-    const navAbout = document.createElement('div')
     navAbout.setAttribute('class', 'about')
+    
+    // Define content for elems.
+    navMenu.innerHTML = `<h1 class="menu">Menu</h1>`
+    navDirections.innerHTML = `<h1 class="directions">Directions</h1>`
     navAbout.innerHTML = `<h1 class="about">About</h1>`
+    
+    // Append all the things.
+    rightDiv.appendChild(navMenu)
+    rightDiv.appendChild(navDirections)
     rightDiv.appendChild(navAbout)
-
+    leftDiv.appendChild(myBlackLogo)
     navBar.appendChild(rightDiv)
     navBar.appendChild(leftDiv)
-
     pageContainer.appendChild(navBar)
 }
 
