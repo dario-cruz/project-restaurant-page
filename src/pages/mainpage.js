e677f696f69f70bb37abf4c1f1b81d2c246ffa61
Dario Cruz <rio2012@gmail.com>
2022-11-28 12:48:56 -0500

    Add css animations to mainpage.

--- src/pages/mainpage.js
+++ src/pages/mainpage.js
@@ -1,4 +1,5 @@
 import './mainpage.css'
+import '../style.css'
 import { pageContainer } from '..'
 import logoImage from '../logo/carbhouse-white.webp'
 
@@ -7,16 +8,20 @@ const renderMainPage = () => {
     // Define elements
     const mainPageDiv = document.createElement('div')
     const chLogo = new Image()
-    chLogo.src = logoImage
+    const textContent = document.createElement('p')
 
     //Set attributes for elements
     mainPageDiv.setAttribute('class', 'mainpage')
     chLogo.setAttribute('id', 'chlogo')
+    chLogo.src = logoImage
+    textContent.setAttribute('class', 'textcontent')
 
+    // Insert content into elements
+    textContent.innerText = "Welcome to CarbHouse the palace of lost gains and a round gut. Please make yourself at home and take a look at the menu."
 
     // Append the elements
     mainPageDiv.appendChild(chLogo)
-
+    mainPageDiv.appendChild(textContent)
 
     // Append to the site page.
     pageContainer.appendChild(mainPageDiv)
