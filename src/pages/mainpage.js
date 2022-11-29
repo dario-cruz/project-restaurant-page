5d4a27f42dc9c52b7e2f6e11efb9fa80983c4ea6
Dario Cruz <rio2012@gmail.com>
2022-11-29 15:45:16 -0500

    Add custom font for menupage.

--- src/pages/mainpage.js
+++ src/pages/mainpage.js
@@ -3,18 +3,23 @@ import '../index.css'
 import { pageContainer } from '..'
 import logoImage from '../logo/carbhouse-white.webp'
 
+// Remove class if there is one.
+document.body.removeAttribute('class')
 
 const renderMainPage = () => {
     // Define elements
     const mainPageDiv = document.createElement('div')
     const chLogo = new Image()
     const textContent = document.createElement('p')
+    const pageBody = document.body
 
     //Set attributes for elements
-    mainPageDiv.setAttribute('class', 'mainpage')
+    mainPageDiv.setAttribute('class', 'mainpagediv')
     chLogo.setAttribute('id', 'chlogo')
     chLogo.src = logoImage
     textContent.setAttribute('class', 'textcontent')
+    pageBody.setAttribute('class', 'mainpage')
+
 
     // Insert content into elements
     textContent.innerText = "Welcome to CarbHouse the palace of lost gains and a round gut. Please make yourself at home and take a look at the menu."
