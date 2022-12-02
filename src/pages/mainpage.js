efb459e6e1efbfb374d8608eee098a1fcbc9f5ca
Dario Cruz <rio2012@gmail.com>
2022-12-02 13:32:03 -0500

    Change mainpage so emojis are on new line.

--- src/pages/mainpage.js
+++ src/pages/mainpage.js
@@ -12,6 +12,7 @@ const renderMainPage = () => {
     const chLogo = new Image()
     const textContent = document.createElement('p')
     const pageBody = document.body
+    const textMojis = document.createElement('p')
 
     //Set attributes for elements
     mainPageDiv.setAttribute('class', 'mainpagediv')
@@ -19,14 +20,15 @@ const renderMainPage = () => {
     chLogo.src = logoImage
     textContent.setAttribute('class', 'textcontent')
     pageBody.setAttribute('class', 'mainpage')
-
+    textMojis.setAttribute('class', 'textcontent')
 
     // Insert content into elements
-    textContent.innerText = "Welcome to CarbHouse the palace of lost gains and a round gut. Please make yourself at home and take a look at the menu and follow us on all the socials!🍞🥐🥖🥪❤️❤️❤️"
-
+    textContent.innerText = "Welcome to CarbHouse the palace of lost gains and a round gut. Please make yourself at home and take a look at the menu and follow us on all the socials!"
+    textMojis.innerText = "🍞🥐🥖🥪❤️❤️❤️"
     // Append the elements
     mainPageDiv.appendChild(chLogo)
     mainPageDiv.appendChild(textContent)
+    mainPageDiv.appendChild(textMojis)
 
     // Append to the site page.
     pageContainer.appendChild(mainPageDiv)
