3cee602ae141462f470a723253ee290ad7b78210
Dario Cruz <rio2012@gmail.com>
2022-12-09 09:16:47 -0500

    Webpack: Add imageminifier plugin to bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackCDNInject = require('webpack-cdn-inject')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin') 
 
 module.exports = {
     mode: 'development',
@@ -29,6 +30,48 @@ module.exports = {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
             },
+        ],
+    },
+    optimization: {
+        minimizer: [
+          "...",
+          new ImageMinimizerPlugin({
+            minimizer: {
+              implementation: ImageMinimizerPlugin.imageminMinify,
+              options: {
+                // Lossless optimization with custom option
+                // Feel free to experiment with options for better result for you
+                plugins: [
+                  ["gifsicle", { interlaced: true }],
+                  ["jpegtran", { progressive: true }],
+                  ["optipng", { optimizationLevel: 5 }],
+                  // Svgo configuration here https://github.com/svg/svgo#configuration
+                  [
+                    "svgo",
+                    {
+                      plugins: [
+                        {
+                          name: "preset-default",
+                          params: {
+                            overrides: {
+                              removeViewBox: false,
+                              addAttributesToSVGElement: {
+                                params: {
+                                  attributes: [
+                                    { xmlns: "http://www.w3.org/2000/svg" },
+                                  ],
+                                },
+                              },
+                            },
+                          },
+                        },
+                      ],
+                    },
+                  ],
+                ],
+              },
+            },
+          }),
         ],
     },
 };
